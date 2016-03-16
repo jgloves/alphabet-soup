922329283983cdd9078638ace7748e742c9d452f
Jessica Glover <glover.jessica.m@gmail.com>
2016-03-16 18:35:32 -0400

    added logic to scramble words

--- alphabet-soup.js
+++ alphabet-soup.js
@@ -1,1 +1,21 @@
 var screen = document.getElementById("screen").getContext("2d");
+
+var words = ["dinosaur", "train"];
+
+scramble(words[1]);
+
+function scramble(word) {
+
+    var letters = word.split("");
+    console.log(letters)
+
+    var scrambledLetters = [];
+    for (i = letters.length; i > 0; i--) {
+        var randomIndex = Math.floor(Math.random() * (i));
+        scrambledLetters.push(letters[randomIndex]); //add a random letter to "scrambledLetters"
+        letters.splice(randomIndex, 1)//remove that letter from "letters"
+    }
+
+    return scrambledLetters;
+
+}
