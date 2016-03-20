63daba8302546bcb2592a9a428fc2b11b7f924b4
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-20 22:24:47 +0000

    format spacing and comments

--- alphabet-soup.js
+++ alphabet-soup.js
@@ -2,15 +2,15 @@
 //TODO - ask Khalid if there is another way, and why this works
 // load google font == Gloria Hallelujah
 WebFontConfig = {
-  google:{ families: ['Gloria Hallelujah'] },
-  active: function(){start();},
+    google:{ families: ['Gloria Hallelujah'] },
+    active: function(){start();},
 };
 
 (function(){
-  var wf = document.createElement("script");
-  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.10/webfont.js';
-  wf.async = 'true';
-  document.head.appendChild(wf);
+    var wf = document.createElement("script");
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.10/webfont.js';
+    wf.async = 'true';
+    document.head.appendChild(wf);
 })();
 
 var screen = document.getElementById("screen").getContext("2d");
@@ -36,20 +36,16 @@ ctx.fillStyle = 'wheat';
 var x = c.width / 2;
 var y = c.height / 2;
 
-
 //TODO- load words from file into list, randomize order
 //for now, hard code list
 var words = ["dinosaur", "train", "bicycle", "music"];
-
 var guessInput = document.getElementById("guess-input");
-
 var guessForm = document.getElementById("guess-form");
 
 //Event listener for submit button
 guessForm.addEventListener("submit", function (e) {
     e = e || event; //workaround for IE
     e.preventDefault();
-
     var guess = guessInput.value;
     if (guess != words[0]) {
         alert("That wasn't the word I was thinking of. Please enter another guess.");
@@ -58,41 +54,29 @@ guessForm.addEventListener("submit", function (e) {
         alert("You got it!");
         guessForm.reset();
     }
-
-
     //TODO - get a new word
-
 });
 
-//for (var i; i < words.length; i++) {
-//    start(i);
-//}
+// we need to grab a new word from our array when the user guesses right
+/* for (var i; i < words.length; i++) {
+    start(i);
+}
+*/
 start(0);
 
-
-
 function start(n){
-
-  var text = scramble(words[n]);
-  ctx.fillText(text, x, y);
-
+    var text = scramble(words[n]);
+    ctx.fillText(text, x, y);
 }
 
 function scramble(word) {
-
     var upperWord = word.toUpperCase(); //TODO - I am getting a strange error here : Uncaught TypeError: Cannot read property 'toUpperCase' of undefined
     var letters = upperWord.split("");
-
     var scrambledLetters = "";
-
     for (var i = letters.length; i > 0; i--) {
         var randomIndex = Math.floor(Math.random() * (i));
         scrambledLetters += letters[randomIndex]; //add a random letter to "scrambledLetters"
-        letters.splice(randomIndex, 1)//remove that letter from "letters"
+        letters.splice(randomIndex, 1); //remove that letter from "letters"
     }
-
     return scrambledLetters;
-
 }
-
-
