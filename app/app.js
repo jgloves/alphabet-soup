2caa63710a96afde46ba399f6ce64da12cd6169c
Jessica Glover <glover.jessica.m@gmail.com>
2016-03-27 12:40:31 -0400

    restructure project

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express();
 // route
 app.get('/', function(req, res) {
-  res.send("Hello world!");
+  res.send({"words": ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "purple", "white", "black" ],
+           "message": "You did it!"}
+);
 });
+
 // listen to server on a port
 app.listen(8080, function(){
   console.log("Listening to server on port 8080");
