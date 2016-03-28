217d4baed2edbce2c411aa9f8652f56a5115252e
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-28 01:32:09 +0100

    refactor server-side logic

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
-// grabbing express
-var express = require('express');
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-// route
-app.get('/', function(req, res) {
-  res.send({"words": ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "purple", "white", "black" ],
-           "message": "You did it!"}
-);
-});
 
-// listen to server on a port
-app.listen(8080, function(){
-  console.log("Listening to server on port 8080");
+// Express
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/x-www-form-urlencoded
+
+// Routes
+app.use('/alphabetsoup', require("./../routes/alphabetsoup"));
+
+// Start Server
+app.listen(8080, function() {
+    console.log("Listening to server on port 8080");
 });
